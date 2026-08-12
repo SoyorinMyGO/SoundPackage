e879327552fd384eaf212cb81818250619a4cc6a
soyorin <gumingdi555@gmail.com>
2026-08-12 23:29:38 +0800

    feat(package): 添加POST /api/package 接口 - 添加"添加语音包的"接口

--- back-end/cruds/package.py
+++ back-end/cruds/package.py
@@ -1,6 +1,7 @@
-from typing import Any, Sequence
+from typing import Any, Sequence, Optional
 
 from sqlalchemy import select
+from sqlalchemy.dialects.mysql import insert
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models.Package import Package
@@ -17,3 +18,21 @@ async def get_list_crud(db: AsyncSession) -> Sequence[Any]:
     result = await db.execute(query)
     package_list = result.scalars().all()
     return package_list
+
+async def add_package_crud(name: str, db: AsyncSession, alias: Optional[str] = None) -> Package:
+    """新建语音包
+
+    Args:
+        name(str): 语音包名
+        alias(str): 语音包别称
+        db(AsyncSession): 数据库会话
+
+    Returns:
+        Package: 新建语音包的信息
+    """
+    package = Package(name=name, alias=alias)
+    db.add(package)
+    await db.commit()
+    await db.refresh(package)
+
+    return package
--- back-end/routers/package.py
+++ back-end/routers/package.py
@@ -1,12 +1,21 @@
-from fastapi import APIRouter, Depends
+from typing import Optional
+
+from fastapi import APIRouter, Depends, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 from configs.db_config import get_db
-from cruds.package import get_list_crud
+from cruds import package
 from utils.response import success_response
 
 router = APIRouter(prefix="/api/package", tags=['package'])
 
 @router.get("")
 async def get_list_router(db: AsyncSession = Depends(get_db)):
-    result = await get_list_crud(db)
+    result = await package.get_list_crud(db)
     return success_response(message='获取语音包列表成功', data=result)
+
+@router.post("")
+async def add_package_router(name:str = Query(...,min_length=1, max_length=50, description='语音包名称'),
+                             alias:Optional[str] = Query(None, min_length=1, max_length=50, description='语音包别称'),
+                             db: AsyncSession = Depends(get_db)):
+    new_package = await package.add_package_crud(name, db, alias)
+    return success_response(message='添加语音包成功', data=new_package)
