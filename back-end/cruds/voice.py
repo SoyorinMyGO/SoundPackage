d8120dd02ef431eb024c147a459c6b9571a8d256
soyorin <gumingdi555@gmail.com>
2026-08-23 10:57:05 +0800

    fix(import): 为获取语音列表添加次级排序(voice.updated_at)

--- back-end/cruds/voice.py
+++ back-end/cruds/voice.py
@@ -1,6 +1,6 @@
 from typing import Any, Sequence
 
-from sqlalchemy import select
+from sqlalchemy import select, desc
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models.Voice import Voice
@@ -15,7 +15,7 @@ async def get_voice_list_crud(db: AsyncSession) -> Sequence[Any]:
     Returns:
         Sequence[Any]: 语音列表
     """
-    query = select(Voice).order_by(Voice.used_times)
+    query = select(Voice).order_by(desc(Voice.used_times)).order_by(desc(Voice.updated_at))
     result = await db.execute(query)
     voice_list = result.scalars().all()
     return voice_list
